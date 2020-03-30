ec18b9d2d160035698b45cf5310f8d60a95f2161
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-03-30 16:31:30 +0100

    challenge 1 complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello from external Javascript file");
+// let myObject;
+
+// myObject = {
+//     a: 10,
+//     b: "abc"
+// }
+
+// console.log(myObject);
+
+// myObject.a = 15;
+
+// console.log(myObject);
+
+// myObject.c = true;
+
+// console.log(myObject);
+
+// delete myObject.b;
+
+// console.log(myObject);
+
+// console.log(myObject.a);
+
+//Challenge 1
+
+let myPost = {};;
+
+myPost.postTitle = "object is reference type";
+
+myPost.postLikes = 0;
+
+myPost.shared = false;
+
+console.log(myPost);
+
+myPost.postLikes =+1;
+
+console.log(myPost);
+
+delete myPost.shared;
+
+console.log(myPost);
