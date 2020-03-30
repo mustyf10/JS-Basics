6d476a744b7a4f5d01b9a9fe6b75481165e2763e
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-03-30 16:50:40 +0100

    bracket notation

--- index.js
+++ index.js
@@ -43,13 +43,31 @@
 
 //Challenge 2
 
-var myObject = {
-    a: 10
-}
+// var myObject = {
+//     a: 10
+// }
 
-var copyOfMyObject = myObject
+// var copyOfMyObject = myObject
 
-copyOfMyObject.b = false;
+// copyOfMyObject.b = false;
+
+// console.log(myObject);
+// console.log(copyOfMyObject);
+
+let myObject = {
+    a: true,
+    b: null,
+    c: 25
+};
+
+console.log(myObject["a"]); //true
+
+console.log(myObject["b"]); //null
+
+const propertyName = "c";
+
+console.log(myObject[propertyName]); //25
+
+myObject["new" + "Property" + "Name"] = "Value for dynamically computed property name";
 
 console.log(myObject);
-console.log(copyOfMyObject);
