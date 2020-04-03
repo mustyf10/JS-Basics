80bfd975f190e3b0632d326d183667585819c8f2
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-04-04 00:20:32 +0100

    comparison operators

--- index.js
+++ index.js
@@ -1,56 +1,111 @@
+// /**
+//  * Section 1
+//  *
+//  * Arithmetic operators
+//  */
+// let a, b;
+// a = 1;
+// b = 2;
+
+// console.log(a + b); //3
+// console.log(a * b); //2
+// console.log(a / b); //0.5
+// console.log(a - b); //-1
+
+// a = "abc";
+// b = 5;
+
+// console.log(a + b); //abc5
+// console.log(a * b); //nan
+// console.log(a / b); //nan
+// console.log(a - b); //nan
+
+// // String concatanation
+// a = "hello";
+// b = "world";
+
+// console.log(a + " " + b); //hello world
+
+// // Unary plus
+// c = "1";
+// console.log(+c); //1
+// c = undefined;
+// console.log(+c); //nan
+
+// // Unary minus
+// c = "5";
+// console.log(-c); //-5
+// c = "abc";
+// console.log(-c); //nan
+
+// // unary ++
+// let d = 5;
+// ++d;
+// console.log(d);
+// d++;
+// console.log(d);
+
+// console.log(++d);
+// console.log(d++);
+// console.log(d);
+
+// // unary --
+// console.log(d--);
+// console.log(d);
+// console.log(--d);
+// console.log(d);
+
 /**
- * Section 1
+ * Section 2
  *
- * Arithmetic operators
+ * Comparison operators
  */
-let a, b;
-a = 1;
-b = 2;
 
-console.log(a + b); //3
-console.log(a * b); //2
-console.log(a / b); //0.5
-console.log(a - b); //-1
+let a, b, c;
+a = 5;
+b = 7;
+c = 5;
 
-a = "abc";
-b = 5;
+console.log(a < b);
+console.log(a > b);
 
-console.log(a + b); //abc5
-console.log(a * b); //nan
-console.log(a / b); //nan
-console.log(a - b); //nan
+console.log(a <= c);
+console.log(a >= c);
 
-// String concatanation
-a = "hello";
-b = "world";
+console.log("_____");
 
-console.log(a + " " + b); //hello world
+// string comparison
+let myStr1, myStr2;
 
-// Unary plus
-c = "1";
-console.log(+c); //1
-c = undefined;
-console.log(+c); //nan
+myStr1 = "abc";
+myStr2 = "bcd";
+myStr3 = "Bcd";
 
-// Unary minus
-c = "5";
-console.log(-c); //-5
-c = "abc";
-console.log(-c); //nan
+console.log(myStr1 > myStr2);
+console.log(myStr1 < myStr2);
+console.log(myStr1 > myStr3);
 
-// unary ++
-let d = 5;
-++d;
-console.log(d);
-d++;
-console.log(d);
+console.log("_____");
 
-console.log(++d);
-console.log(d++);
-console.log(d);
+// equality operators
+let myString = "0";
+let myNumber = 0;
+let myBoolean = false;
 
-// unary --
-console.log(d--);
-console.log(d);
-console.log(--d);
-console.log(d);
+console.log(myString == myNumber);
+console.log(myNumber == myBoolean);
+console.log(myBoolean == myString);
+
+// strict equality operator - type and value are compared
+console.log(myString === myNumber);
+console.log(myNumber === myBoolean);
+console.log(myBoolean === myString);
+
+console.log(myString != myNumber);
+console.log(myString !== myNumber);
+
+console.log("_____");
+
+console.log(null === undefined);
+console.log(null == undefined); //both become the number 0
+console.log((((0 === "") === null) === undefined) === false);
