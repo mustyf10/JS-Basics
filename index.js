0410f694e3dbd3ea2e460f4a224f07297723c31e
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-04-03 23:48:27 +0100

    arithmetic operators

--- index.js
+++ index.js
@@ -1,208 +1,56 @@
-// /**
-//  * Example 1
-//  *
-//  * Simplest function in JS
-//  */
-
-// function myFunction() {}
-
-// console.log(myFunction);
-
-// myFunction();
-
-// console.log(myFunction()); //undefined
-
-// console.log(myFunction(10, true)); //undefined
-
-// /**
-//  * Example 2
-//  *
-//  * Parameters of a function
-//  */
-
-//  function myFunction(a,b) {
-//      console.log(a);
-//      console.log(b);
-//  }
-
-//  myFunction(10, 5);
-
-//  myFunction(true, null);
-
-//  console.log(myFunction(4, 3)); //undefined - nothing returned
-
-// /**
-//  * Example 3
-//  *
-//  * Function scope
-//  */
-
-//  function myFunction(a,b ){
-//      console.log(a, b);
-//  }
-
-//  console.log(myFunction(3, 2));
-
-//  console.log(a);
-
-// /**
-//  * Example 4
-//  *
-//  * Reuse parameter names
-//  */
-
-//  function myFunction1(a ,b) {
-//      console.log(a, b);
-//  }
-
-//  function myFunction2(a, b) {
-//      console.log(a, b);
-//  }
-
-//  const a = true;
-//  const b = null;
-
-//  myFunction1(2, 3);
-
-//  myFunction2(5, 7);
-
-//  console.log(a, b);
-
-// /**
-//  * Example 5
-//  *
-//  * Add return
-//  */
-
-//  function sum(a, b){
-//      return a + b;
-//  }
-
-//  sum(10, 5);
-
-// /**
-//  * Example 6
-//  *
-//  * What happens after return?
-//  */
-
-//  function myFunction(a){
-//     console.log(a);
-//     return a; //function stops here
-//     console.log(a);
-// }
-
-// myFunction(10);
-
-// /**
-//  * CHALLENGE 1
-//  *
-//  * Create function with 3 parameters
-//  * Create new variable and assign result
-//  * Print to the console the result
-//  * No return
-//  */
-
-//  function multiplication(a, b, c){
-//     var result = a * b * c;
-//     console.log(result);
-//  }
-
-//  multiplication(2, 2, 2);
-
-// /**
-//  * CHALLENGE 2
-//  *
-//  * Create function called 'concatenateStrings' and will have 2 parameters
-//  * this function will retrun concatenated strings
-//  */
-
-// function concatenateStrings(a, b){
-//     return a + " " + b;
-// }
-
-// const c = concatenateStrings("Hello", "World");
-
-// console.log(c);
-
-// /**
-//  * CHALLENGE 3
-//  *
-//  * Create function called outerFunction with 2 paramenters
-//  * Create function called innerFunction with 1 parameter
-//  * innerFunction will return square of parameter
-//  * outerFunction will return sum of parameters
-//  * call innerFunction with argument sum of both parameters from outerFunction
-//  * log to the console result innerFunction
-//  */
-
-// function outerFunction(a, b) {
-//   function innerFunction(c) {
-//     return c * c;
-//   }
-//   const c = a + b;
-//   console.log(innerFunction(c));
-// }
-
-// outerFunction(2, 3);
-
-// outerFunction(5, 5);
-
-// outerFunction(7, 3);
-
-// /**
-//  * ANONYMOUS FUNCTION EXPRESSIONS
-//  */
-
-// /**
-//  * Example 1
-//  *
-//  */
-
-// const myFunction = function() {}; //myFunction holds function as a value
-// console.log(myFunction); //can call the value
-
-// /**
-//  * Example 2
-//  *
-//  * Callback function
-//  */
-
-// setTimeout(function() {
-//   console.log("Delayed Message");
-// }, 3000);
-
-// /**
-//  * Example 3
-//  *
-//  * Callback function (ii)
-//  */
-
-// let i = 1;
-// setInterval(function() {
-//   console.log("log each one second " + i);
-//   i = i + 1;
-// }, 1000);
-
 /**
- * CHALLENGE
+ * Section 1
  *
- * Function Expressions
- *
- * Use following built-in functions:
- * setTimeout
- * setInterval
- * clearInterval
- *
- * Print to the console only 5 messages with 2 second intervals
+ * Arithmetic operators
  */
+let a, b;
+a = 1;
+b = 2;
 
-var i = 1;
-const myInterval = setInterval(function() {
-  console.log("Here is message number " + i);
-  i = i + 1;
-}, 2000);
+console.log(a + b); //3
+console.log(a * b); //2
+console.log(a / b); //0.5
+console.log(a - b); //-1
 
-setTimeout(function() {
-  clearInterval(myInterval);
-}, 10000);
+a = "abc";
+b = 5;
+
+console.log(a + b); //abc5
+console.log(a * b); //nan
+console.log(a / b); //nan
+console.log(a - b); //nan
+
+// String concatanation
+a = "hello";
+b = "world";
+
+console.log(a + " " + b); //hello world
+
+// Unary plus
+c = "1";
+console.log(+c); //1
+c = undefined;
+console.log(+c); //nan
+
+// Unary minus
+c = "5";
+console.log(-c); //-5
+c = "abc";
+console.log(-c); //nan
+
+// unary ++
+let d = 5;
+++d;
+console.log(d);
+d++;
+console.log(d);
+
+console.log(++d);
+console.log(d++);
+console.log(d);
+
+// unary --
+console.log(d--);
+console.log(d);
+console.log(--d);
+console.log(d);
