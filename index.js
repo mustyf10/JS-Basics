3380b9352e6b1c724978f4e50f7cd1aa900d4751
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-04-05 18:57:02 +0100

    function scopes

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
+// /**
+//  *
+//  * SCOPES
+//  *
+//  * Example 1
+//  *
+//  * Global Scope
+//  *
+//  */
+// var a = 10;
+// let b = true; // let variables are not added to the global object proeprties
+
+// console.log(a);
+// console.log(window.b);
+
+// const c = "abc"; //const also not attached to global object properties but still a global scope variable
+
+// function myFunction(a){
+//     console.log(a);
+// }
+
 /**
+ * Example 2
  *
- * SCOPES
- *
- * Example 1
- *
- * Global Scope
- *
+ * Function scope
  */
-var a = 10;
-let b = true; // let variables are not added to the global object proeprties
+let e;
 
-console.log(a);
-console.log(window.b);
-
-const c = "abc"; //const also not attached to global object properties but still a global scope variable
-
-function myFunction(a){
-    console.log(a);
+function myFunction(a, b) {
+  const c = true;
+  console.log(c);
+  //console.log(d) undefined value d not in the function or global scope
+  console.log(e);
+  console.log(a, b); //different values depending on what is passed through
 }
+
+myFunction();
+
+myFunction(2, 3);
