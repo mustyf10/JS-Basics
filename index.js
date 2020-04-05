d058820ad02c49f89a9b5146ea60d67a257b0e30
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-04-05 18:51:54 +0100

    global scope

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 /**
  *
- *
  * SCOPES
  *
+ * Example 1
+ *
+ * Global Scope
+ *
  */
+var a = 10;
+let b = true; // let variables are not added to the global object proeprties
+
+console.log(a);
+console.log(window.b);
+
+const c = "abc"; //const also not attached to global object properties but still a global scope variable
+
+function myFunction(a){
+    console.log(a);
+}
