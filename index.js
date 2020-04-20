4c34560eeaed45ad3ce3168b848e76ba7d5e7679
Mustafa Fajandar <mustafa7_95@hotmail.com>
2020-04-20 10:40:22 +0100

    example 2

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 /**
  * ARRAYS
- * 
+ *
  * EXAMPLE 1
  * EMPTY ARRAYS
  */
+// const myArray = [];
+// console.log(myArray);
+
+/**
+ * Example 2
+ *
+ * Add elements to the array
+ */
 const myArray = [];
+
+myArray[0] = "First element";
+console.log(myArray);
+
+myArray[1] = "Second element";
+console.log(myArray);
+
+myArray.push("Third element");
 console.log(myArray);
